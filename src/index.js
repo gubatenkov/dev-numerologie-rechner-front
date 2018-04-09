da7d3f80545a1888630add6525f3f4b18c61f1c6
Christoph Hechenblaikner <christoph@crosscloud.me>
2018-04-09 11:30:54 +0200

    switched bach to hardcoded endpoint

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ import './styles/font-awesome/font-awesome.min.css';
 import './styles/theme.css';
 
 // graphql endpoint url
-const GRAPHQL_HOST = process.env.BACKEND_HOST || 'http://localhost:4000';
-// const GRAPHQL_HOST = 'https://dev-numerologie-rechner.herokuapp.com';
+// const GRAPHQL_HOST = process.env.BACKEND_HOST || 'http://localhost:4000';
+const GRAPHQL_HOST = 'https://dev-numerologie-rechner.herokuapp.com';
 const GRAPHQL_ENDPOINT = `${GRAPHQL_HOST}/graphql`;
 
 // creating fragment matcher
