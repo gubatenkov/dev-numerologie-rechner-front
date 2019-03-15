49af5b38918865d8772c22bd68c9910e21c29b63
Philipp Pali-Rausch <pali-rausch@denovo.at>
2019-03-15 08:55:45 +0100

    Forwarding

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,5 @@
 // graphql endpoint url configuration
- //export const GRAPHQL_HOST = process.env.BACKEND_HOST || 'http://localhost:4000';
+// export const GRAPHQL_HOST = process.env.BACKEND_HOST || 'http://localhost:4000';
 // export const GRAPHQL_HOST = 'https://dev-numerologie-rechner.herokuapp.com';
 export const GRAPHQL_HOST = 'https://prd-numerologie-rechner.herokuapp.com';
 export const GRAPHQL_ENDPOINT = `${GRAPHQL_HOST}/graphql`;
