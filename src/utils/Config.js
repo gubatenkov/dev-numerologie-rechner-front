a4b6225052f491f013e0cc184907ffa0e0d566f8
Mikhail Neverov <mneveroff@devguild.ru>
2019-04-29 00:49:26 +0700

    Added proper react app  backend host url

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,4 @@
 // graphql endpoint url configuration
-// export const GRAPHQL_HOST = process.env.BACKEND_HOST || 'http://localhost:4000';
-// export const GRAPHQL_HOST = 'https://dev-numerologie-rechner.herokuapp.com';
-export const GRAPHQL_HOST = 'https://prd-numerologie-rechner.herokuapp.com';
+export const GRAPHQL_HOST = process.env.REACT_APP_BACKEND_HOST || 'https://prd-numerologie-rechner.herokuapp.com';
 export const GRAPHQL_ENDPOINT = `${GRAPHQL_HOST}/graphql`;
 export const AUTH_ENDPOINT = `${GRAPHQL_HOST}/auth`;
