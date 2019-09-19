e6a4950b8ebc9f77c85ad1781f2bf11ece438af7
Christoph Winter <christoph.hech@gmail.com>
2019-09-19 07:28:37 +0200

    fixed complete purchase handling

--- src/components/CreditsBuyWait.jsx
+++ src/components/CreditsBuyWait.jsx
@@ -3,46 +3,82 @@ import { Query } from 'react-apollo';
 import { currentWindowToken } from '../graphql/Queries';
 import LoadingIndicator from './LoadingIndicator';
 
+// default refresh interval in seconds
+const DEFAULT_REFRESH_INTERVAL = 10;
+
+
+/**
+ * Hook that periodically checks for updates in currentWindowToken
+ * and calls a callback if an update has been detected
+ * @param {function} callback callback function to be called every delay seconds
+ * @param {int} delay time interval to call callback in (ms)
+ */
 function useInterval(callback, delay) {
+  // callback as ref value
   const savedCallback = useRef();
 
-  // Remember the latest callback.
+  // effect that saves the callback passed as prop (if prop changed)
   useEffect(() => {
+    // saving updated callback
     savedCallback.current = callback;
   }, [callback]);
 
-  // Set up the interval.
+  // effect that sets up periodic task to call callback (if delay changed)
   useEffect(() => {
+    // function called all delay seconds
     function tick() {
+      // calling provided callback
       savedCallback.current();
     }
+
+    // if a delay has been passed => starting periodic task
     if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
+      // set up task every delay second
+      const intervalId = setInterval(tick, delay);
+
+      // cleanup function => clear interval
+      return () => {
+        clearInterval(intervalId);
+      };
     }
+    // return null;
   }, [delay]);
 }
 
+/**
+ * Wait component that periodically checks passed refetch function and calls onSuccess
+ * if provided data contains all required data.
+ */
 const Wait = ({ onSuccess, data, loading, refetch }) => {
+  // if sufficient data is present => calling provided callback
   if (data && data.windowToken && data.windowToken.wpOrderId) {
     onSuccess();
   }
 
+  // using useInteval hook to referch periodically
   useInterval(() => {
     refetch();
-  }, 1000 * 10);
+  }, DEFAULT_REFRESH_INTERVAL * 1000);
 
+  // returning loading indicator if waiting
   return (
     <LoadingIndicator text={`Wir warten bis die Zahlung in unserem Webshop (in einem separaten Fenster) erfolgreich durchgeführt wurde.`} />
   );
 };
 
+// exporting wrapped query component
+// TODO: Use apollo polling
 export default ({ windowToken, onSuccess }) => {
+  // querying for a window token and rendering wait component with result
   return (
     <Query query={currentWindowToken} variables={{ windowToken }}>
-      {({ data, loading, refetch }) => (
-         <Wait {...{ onSuccess, data, loading, refetch }} />
-      )}
+      {({ loading, data, refetch }) => {
+        return (
+          <Wait {...{
+            onSuccess, data, loading, refetch,
+          }}
+          />);
+      }}
     </Query>
   );
 }
--- src/utils/buyCredits.js
+++ src/utils/buyCredits.js
@@ -1,26 +1,9 @@
+// webshop product ids of short and long personal analysis credit
 const CREDIT_PERSONAL_SHORT_WPID = 364;
 const CREDIT_PERSONAL_LONG_WPID  = 365;
 
 const baseUrl = 'https://www.bios-shop.eu';
-const loginUri = `${baseUrl}/?remote_login=true`;
-
-/**
- * Opens a browser window with shop, add the products identified
- * by passed ids to cart and navigate to cart
- * @param {Array} productIds An array of product id strings of all products to add to the cart. 
- * e.g. ['42', '42', '55', '63'].
- */
-function addProductsToShopCart(productIds) {
-  // generating string of productIds to add include in the URI
-  const productIDsURI = encodeURIComponent(productIds.join(','));
-
-  // generating add to cart URL for products
-  const addToCartURI = `${baseUrl}/warenkorb/?add_to_cart_multiple=${productIDsURI}`;
-
-  // opening and focusing link
-  const shopWindow = window.open(addToCartURI, '_blank');
-  shopWindow.focus();
-}
+// const loginUri = `${baseUrl}/?remote_login=true`;
 
 /**
  * Authenticates the user in the woocommerce webshop and redirects to the
@@ -31,17 +14,47 @@ function addProductsToShopCart(productIds) {
  * @param {*} windowToken TBA
  */
 /* function authenticateAndRedirectToUrl(redirectUri, wpAccessToken, windowToken) {
-  const url = `${loginUri}&window_token=${windowToken}&access_token=
-  ${wpAccessToken}&redirect_uri=${redirectUri}`;
+  const url = `${loginUri}&window_token=${windowToken}&access_token=${wpAccessToken}&redirect_uri=${redirectUri}`;
   const win = window.open(url, '_blank');
   win.focus();
 } */
 
+/**
+ * Opens a browser window with shop, add the products identified
+ * by passed ids to cart and navigate to cart
+ * @param {Array} productIds An array of product id strings of all products to add to the cart. 
+ * e.g. ['42', '42', '55', '63'].
+ * @param {String} windowToken A unique token identifying the new shop window. This is used by the shop to match the window
+ * with an order number upon completion in the database. So windowToken and wpOrderId will be matched in the db
+ */
+function addProductsToShopCart(productIds, windowToken) {
+  // generating string of productIds to add include in the URI
+  const productIDsURI = encodeURIComponent(productIds.join(','));
+
+  // generating add to cart URL for products
+  const addToCartURI = `${baseUrl}/warenkorb/?add_to_cart_multiple=${productIDsURI}&window_token=${windowToken}`;
+
+  // opening and focusing link
+  const shopWindow = window.open(addToCartURI, '_blank');
+  shopWindow.focus();
+}
+
+/**
+ * opens a browser window for the shop and adds the selected credits to the shop
+ * @param {Integer} personalShorts Number of short credits to buy in the shop
+ * @param {Integer} personalLongs Number of long credits to buy in the shop
+ * @param {*} wpAccessToken A wordpress access token to log the user in on the webshop (not yet implemented)
+ * @param {*} windowToken A unique window token for the shop to match with an order ID upon completion.
+ * Once a purchase is completed, this windowId will be matched with an wpOrderId in the db marking completion
+ * of the purchase.
+ */
 export default function buyCredits(personalShorts = 0, personalLongs = 0, wpAccessToken, windowToken) {
+  // getting array of ids for short and long credit products
   const idsPersonalShorts = Array(parseInt(personalShorts, 10)).fill(CREDIT_PERSONAL_SHORT_WPID);
   const idsPersonalLongs = Array(parseInt(personalLongs, 10)).fill(CREDIT_PERSONAL_LONG_WPID);
-  const ids = [];
 
+  // constructing array of all product ids
+  const ids = [];
   if (idsPersonalShorts) {
     ids.push(...idsPersonalShorts);
   }
@@ -49,5 +62,6 @@ export default function buyCredits(personalShorts = 0, personalLongs = 0, wpAcce
     ids.push(...idsPersonalLongs);
   }
 
-  addProductsToShopCart(ids);
+  // opening shop window and adding products to cart
+  addProductsToShopCart(ids, windowToken);
 }
