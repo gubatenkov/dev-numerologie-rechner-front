aaf212aa631c1da66d239e3372c56c003eb9709d
askoki <arian.skoki@cnc.io>
2019-12-16 13:03:20 +0100

    feature/AKB-8-my-profile-page: Shown email info in user settings

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
+import { currentUserQuery } from '../graphql/Queries';
+import { useQuery } from '@apollo/react-hooks';
 
-class UserProfile extends Component {
-  render() {
-    return (
+function UserProfile() {
+  const { data } = useQuery(currentUserQuery);
+
+  if (!data) return null;
+
+  return (
+    <div>
       <div>
-        <div>
-          <NavigationBar />
-        </div>
-        <div>
-          <h2>Personal Information</h2>
-          <h5>Aktueller Name</h5>
-          <input type="text" name="firstName" />
-          <input type="text" name="lastName" />
-          <h5>Email</h5>
-          <input type="text" name="email" disabled={true} />
-          <button type="submit">Save</button>
-        </div>
+        <NavigationBar />
       </div>
-    );
-  }
+      <div>
+        <h2>Personal Information</h2>
+        <h5>Aktueller Name</h5>
+        <input type="text" name="firstName" />
+        <input type="text" name="lastName" />
+        <h5>Email</h5>
+        <input
+          type="text"
+          name="email"
+          disabled={true}
+          value={data.currentUser.email}
+        />
+        <button type="submit">Save</button>
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(UserProfile);
