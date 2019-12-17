0ba7beba9f1563c9606f634c6648532a98221c9c
askoki <arian.skoki@cnc.io>
2019-12-17 14:12:19 +0100

    feature/AKB-8-my-profile-page: Refactored navigation dropdown to support custom toggle components

---
+++ src/components/Dropdowns/DropdownMenuAddUtils.jsx
@@ -0,0 +1,7 @@
+import { ReactComponent as IconAdd } from '../../images/icon_add.svg';
+import React from 'react';
+
+const addToggleStyle = {
+  cursor: 'pointer',
+};
+export const AddToggleIcon = <IconAdd style={addToggleStyle} />;
