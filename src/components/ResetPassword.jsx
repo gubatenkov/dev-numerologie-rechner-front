6a80aa083c4c213c7c5d8dabdcd050ae62f26e1f
Stefan Papst <stefan@fasky.io>
2020-05-07 18:59:53 +0200

    further translations

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { withRouter, Link } from "react-router-dom";
 import ToastNotifications from "cogo-toast";
 import LoadingIndicator from "./LoadingIndicator";
-
+import { useTranslation } from "react-i18next";
 import { postJsonData } from "../utils/AuthUtils";
 
 import Panel from "./Panel";
@@ -11,60 +11,46 @@ import InputField from "./InputField";
 import logo from "../images/logo_weiss_trans.png";
 import "../styles/InputForm.css";
 
-// delay after which the user is redirected upon successful reset
 const DELAY_REDIRECT_AFTER_RESET = 3000;
 
 const ResetPassword = props => {
-  // getting prop elements
+  const { t } = useTranslation();
   const { history } = props;
 
-  // email address and loading state
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
   // WORKAROUND: setting background of whole doc upon mount/unmount
   useEffect(() => {
-    // setting background dynamically
     document.body.style.backgroundColor = "#00b3d4";
 
     return () => {
-      // resetting background dynamically
       document.body.style.backgroundColor = null;
     };
   });
 
-  // attempt to reset password on server
   const resetPassword = async () => {
-    // sending request to server
     try {
-      // setting loading indicator
       setLoading(true);
-
-      // making request to server
       await postJsonData("/reset-password", {
         email
       });
 
-      ToastNotifications.success(
-        "Ein Email mit einer Anleitung zum Zurücksetzten des Passworts wurde versendet.",
-        { position: "top-right" }
-      );
+      ToastNotifications.success(t("EMAIL_WAS_SENT"), {
+        position: "top-right"
+      });
 
       // redirecting to user home after delay
       setTimeout(() => history.push("/login"), DELAY_REDIRECT_AFTER_RESET);
     } catch (error) {
-      ToastNotifications.error(
-        "Rücksetzen fehlgeschlagen. Bitte versuchen Sie es erneut.",
-        { position: "top-right" }
-      );
+      ToastNotifications.error(t("RESET_FAILED"), { position: "top-right" });
     } finally {
       setLoading(false);
     }
   };
 
-  // if loading => Showing indicator to user
   if (loading) {
-    return <LoadingIndicator text="Sende Email..." />;
+    return <LoadingIndicator text={t("SEND_EMAIL")} />;
   }
 
   return (
@@ -72,7 +58,7 @@ const ResetPassword = props => {
       <div className="page vertical-align">
         <div className="page-content">
           <div className="text-center">
-            <a href="https://www.psychologischenumerologie.eu/">
+            <a href={t("HOMEPAGE")}>
               <img
                 className="brand-img logo"
                 height="250"
@@ -83,10 +69,10 @@ const ResetPassword = props => {
           </div>
           <div className="row justify-content-md-center">
             <div className="col-lg-4">
-              <Panel title="Passwort Zurücksetzen">
+              <Panel title={t("PASSWORD_RESET")}>
                 <InputField
                   icon="wb-user"
-                  fieldName="Email-Adresse"
+                  fieldName={t("EMAIL")}
                   onChange={event => setEmail(event.target.value)}
                 />
                 <button
@@ -94,14 +80,14 @@ const ResetPassword = props => {
                   className="btn btn-primary btn-block"
                   onClick={resetPassword}
                 >
-                  Passwort Zurücksetzen
+                  {t("PASSWORD_RESET")}
                 </button>
                 <div className="InputForm__options">
                   <Link to="/login">
-                    <h6>Anmelden</h6>
+                    <h6>{t("SIGN_IN")}</h6>
                   </Link>
                   <Link to="/register">
-                    <h6>Registrieren</h6>
+                    <h6>{t("REGISTER")}</h6>
                   </Link>
                 </div>
               </Panel>
