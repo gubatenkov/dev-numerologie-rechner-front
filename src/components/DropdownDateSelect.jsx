f115cabb72fd07b284f58ac221573364f5aad7ef
s9379992g <s9379992g@gmail.com>
2022-03-31 16:00:38 +0300

    added own version of package react-dropdown-date-de to npm; used in ./DropdownDateSelect.jsx

--- src/components/DropdownDateSelect.jsx
+++ src/components/DropdownDateSelect.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
-import { YearPicker, MonthPicker, DayPicker } from "react-dropdown-date";
+import { YearPicker, MonthPicker, DayPicker } from "react-dropdown-date-de";
 
 const DropdownDateSelect = ({ date, setDate }) => {
   return (
