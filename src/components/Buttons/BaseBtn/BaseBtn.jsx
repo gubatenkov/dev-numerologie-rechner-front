7afcbcb38c73c32b2996e2da4286fc3f20931128
s9379992g <s9379992g@gmail.com>
2022-05-04 20:35:05 +0300

    added spinner to Login btn to spinn when user logging in

--- src/components/Buttons/BaseBtn/BaseBtn.jsx
+++ src/components/Buttons/BaseBtn/BaseBtn.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
 import "./index.scss";
 
-const Btn = ({ children, link = false, href, ...restProps }) => {
-  if (link) {
+const Btn = ({ children, link = false, href, loading, ...restProps }) => {
+  if (loading) {
+    return (
+      <button {...restProps} disabled>
+        <Spinner animation="border" role="status" variant="light" />
+      </button>
+    );
+  } else if (link) {
     return (
       <Link to={href} {...restProps}>
         {children}
